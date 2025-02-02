f4bd4cb93075af6578e2a6f86c360397bb0e1601
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2025-02-02 11:01:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,7 +24,7 @@
 
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i 's|https://github.com/qosmio/nss-packages.git;NSS-12.5-K6.x|https://github.com/LiBwrt/nss-packages.git|' feeds.conf.default
+# sed -i 's|https://github.com/qosmio/nss-packages.git;NSS-12.5-K6.x|https://github.com/LiBwrt/nss-packages.git|' feeds.conf.default
 
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
